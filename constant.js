9a9f70d36ac36d776768cefe95645e18f6940417
mar-babu <ar_cse@yahoo.com>
2020-07-12 22:02:16 +0600

    constant

---
+++ constant.js
@@ -0,0 +1,10 @@
+                //Constant
+
+ //let interestRate = 0.3;
+ //interestRate = 1;  //variable are restoring
+
+
+ const interestRate = 0.3;
+ interestRate = 1;  //variable are restoring not possible
+
+ console.log(interestRate);
